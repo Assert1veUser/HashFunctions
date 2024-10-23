6625952822ad4aab57040015188e0e3097639bb3
Assert1veUser <tony.markin2017@yandex.ru>
2024-10-23 14:38:40 +0300

    Changes to be committed: 	modified:   HashFunctions.sln 	new file:   Task2/Program.cs 	new file:   Task2/bin/Debug/net8.0/task2.deps.json 	new file:   Task2/bin/Debug/net8.0/task2.dll 	new file:   Task2/bin/Debug/net8.0/task2.exe 	new file:   Task2/bin/Debug/net8.0/task2.pdb 	new file:   Task2/bin/Debug/net8.0/task2.runtimeconfig.json 	new file:   Task2/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs 	new file:   Task2/obj/Debug/net8.0/Task2.AssemblyInfoInputs.cache 	new file:   Task2/obj/Debug/net8.0/Task2.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Task2/obj/Debug/net8.0/Task2.GlobalUsings.g.cs 	new file:   Task2/obj/Debug/net8.0/Task2.assets.cache 	new file:   Task2/obj/Debug/net8.0/apphost.exe 	new file:   Task2/obj/Debug/net8.0/ref/task2.dll 	new file:   Task2/obj/Debug/net8.0/refint/task2.dll 	new file:   Task2/obj/Debug/net8.0/task2.csproj.CoreCompileInputs.cache 	new file:   Task2/obj/Debug/net8.0/task2.csproj.FileListAbsolute.txt 	new file:   Task2/obj/Debug/net8.0/task2.dll 	new file:   Task2/obj/Debug/net8.0/task2.genruntimeconfig.cache 	new file:   Task2/obj/Debug/net8.0/task2.pdb 	new file:   Task2/obj/Debug/net8.0/task2.sourcelink.json 	new file:   Task2/obj/Task2.csproj.nuget.g.props 	new file:   Task2/obj/Task2.csproj.nuget.g.targets 	new file:   Task2/obj/project.assets.json 	new file:   Task2/obj/project.nuget.cache 	new file:   Task2/obj/task2.csproj.nuget.dgspec.json 	new file:   Task2/task2.csproj 	modified:   task1/bin/Debug/net8.0/task1.dll 	modified:   task1/bin/Debug/net8.0/task1.exe 	modified:   task1/bin/Debug/net8.0/task1.pdb 	modified:   task1/obj/Debug/net8.0/apphost.exe 	modified:   task1/obj/Debug/net8.0/ref/task1.dll 	modified:   task1/obj/Debug/net8.0/refint/task1.dll 	modified:   task1/obj/Debug/net8.0/task1.AssemblyInfo.cs 	modified:   task1/obj/Debug/net8.0/task1.AssemblyInfoInputs.cache 	modified:   task1/obj/Debug/net8.0/task1.csproj.FileListAbsolute.txt 	modified:   task1/obj/Debug/net8.0/task1.dll 	modified:   task1/obj/Debug/net8.0/task1.genruntimeconfig.cache 	modified:   task1/obj/Debug/net8.0/task1.pdb 	new file:   task1/obj/Debug/net8.0/task1.sourcelink.json

---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Program
+{
+    public static string Md5(string input)
+    {
+        string hashValue;
+        MD5 MD5Hash = MD5.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+        byte[] hash = MD5Hash.ComputeHash(inputBytes);
+        hashValue = Convert.ToHexString(hash);
+
+        return hashValue;
+    }
+
+    public static void Decrypt()
+    {
+        Console.WriteLine("Введите длину сообщения");
+        int length = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите hash");
+        string hash = Console.ReadLine();
+        char[] chars = new char[length];
+
+        void Fun(char[] chars_, int count)
+        {
+            for (int j = 32; j <= 1104; j++)
+            {
+                chars[count] = (char)j;
+                if (count + 1 < chars_.Length)
+                    Fun(chars, count + 1);
+                string str = new string(chars_);
+                if (Md5(str).Contains(hash))
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+
+        Fun(chars, 0);
+    }
+
+    public static void Main()
+    {
+        Decrypt();
+    }
+}
