cea904b3b2506a4b9c707ef07c11d052ba97a5e6
Assert1veUser <tony.markin2017@yandex.ru>
2024-10-23 11:03:07 +0300

    Changes to be committed: 	modified:   task1/obj/Debug/net8.0/task1.AssemblyInfo.cs 	modified:   task1/obj/Debug/net8.0/task1.AssemblyInfoInputs.cache 	modified:   task1/obj/Debug/net8.0/task1.GeneratedMSBuildEditorConfig.editorconfig 	modified:   task1/obj/Debug/net8.0/task1.assets.cache 	modified:   task1/obj/project.assets.json 	modified:   task1/obj/project.nuget.cache 	modified:   task1/obj/task1.csproj.nuget.dgspec.json

--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe33ec80c9c05884a2480c4a6244237ec5ca527")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
